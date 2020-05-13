0460a2d140bc41ba4f3cae5be86e177031788eb3
marcelorafael <marcelorafael23@hotmail.com>
2020-05-13 18:15:22 -0300

    Pequenas mudanças no estudos01

--- estudo01/src/App.js
+++ estudo01/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Equipe extends Component{
+    render(){
+        return(
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
+            </div>
+        );
+    }
+}
+
+class Sobre extends Component{
+    render(){
+        return(
+            <div>
+                <p> Fale jhow, sou o {this.props.nome} </p>
+                <p> {this.props.cargo} </p>
+                <p> {this.props.idade} </p>
+            </div>
+        )
+    }
+}
+
+function App(){
+    return(
+        <div>
+            <h1> Conheça nossa equipe: </h1>
+            <Equipe nome="Marcelo" cargo="Programador" idade="32" />
+
+        </div>
+    );
 }
 
 export default App;
